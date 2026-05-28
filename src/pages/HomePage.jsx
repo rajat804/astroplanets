962706d1ef09c2577138c650216d7bc63e48b45c
Rajat <rajat804@gmail.com>
2026-05-28 14:27:15 +0530

    hom page udpate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,12 +10,19 @@ import Testimonials from "../components/home/Testimonials";
 import FAQ from "../components/home/FAQ";
 import BlogPreview from "../components/home/BlogPreview";
 import VastuCalendar from "../components/VastuCalendar";
+// import SectionDivider from "../components/SectionDivider";
+import PlanetSigns from "../components/home/PlanetSigns";
 
 export default function HomePage() {
   return (
     <div className="bg-offWhite text-gray-900">
       <Hero />
+      {/* <SectionDivider variant="wave" /> */}
+
+    {/* Planet Signs Section - Add this right after Hero */}
+      <PlanetSigns />
       <Stats />
+         <div className="h-4 bg-gradient-to-b from-white/0 to-white"></div>
       <VastuCalendar />
       <Services />
       <Products />
