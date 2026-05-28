f989164b765c9ea086ed1e7caf6e034b5278392f
Rajat <rajat804@gmail.com>
2026-05-28 14:26:56 +0530

    hero

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -23,7 +23,7 @@ import BookConsultationButton from "../common/BookConsultationButton";
 import SliderComponent from "../common/SliderComponent";
 
 const Hero = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
   const [isMobile, setIsMobile] = useState(false);
   const [isLowEndDevice, setIsLowEndDevice] = useState(false);
 
@@ -473,6 +473,21 @@ const Hero = () => {
           .will-change-transform { will-change: transform; }
         `}</style>
       </section>
+  {/* Add transition divider at bottom of Hero */ }
+  {/* <div className="absolute bottom-0 left-0 right-0">
+    <svg
+      className="w-full h-12 md:h-16"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none"
+    >
+      <path
+        d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+        fill="#ffffff"
+        opacity="0.1"
+      />
+    </svg>
+  </div> */}
     </>
   );
 };
