e80a987f5b12d0a05dafde6a7824a97465058189
Rajat <rajat804@gmail.com>
2026-06-16 19:21:32 +0530

    instructor

--- src/components/home/Instructors.jsx
+++ src/components/home/Instructors.jsx
@@ -41,7 +41,7 @@ const Instructors = () => {
     message: ""
   });
 
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  const API_URL = import.meta.env.VITE_API_URL;
 
   // Fetch experts from API
   useEffect(() => {
